afda0810aceeaf2e33d25424f923ce4027657fea
Giovanna Melo <gimelosilva@outlook.com>
2022-06-09 16:59:28 -0300

    Atualização

--- src/main/java/com/generation/lojagames/service/UsuarioService.java
+++ src/main/java/com/generation/lojagames/service/UsuarioService.java
@@ -3,14 +3,16 @@ package com.generation.lojagames.service;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
-import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import com.generation.lojagames.model.Usuario;
 import com.generation.lojagames.model.UsuarioLogin;
 import com.generation.lojagames.repository.UsuarioRepository;
 
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
 
 	@Autowired
